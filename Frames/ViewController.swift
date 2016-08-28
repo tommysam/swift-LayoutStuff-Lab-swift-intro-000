d5fcf90a5d73eabd1f8ff886163535b26d796eda
Michael Dippery <michael@monkey-robot.com>
2016-08-27 21:57:12 -0700

    Initialize sliders and labels

--- Frames/ViewController.swift
+++ Frames/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var frameXSlider: UISlider!
+    @IBOutlet weak var frameYSlider: UISlider!
+    @IBOutlet weak var boundsXSlider: UISlider!
+    @IBOutlet weak var boundsYSlider: UISlider!
+
+    @IBOutlet weak var frameXLabel: UILabel!
+    @IBOutlet weak var frameYLabel: UILabel!
+    @IBOutlet weak var boundsXLabel: UILabel!
+    @IBOutlet weak var boundsYLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateLabels()
+        updateSliders()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func updateLabels() {
+        let frame = imageView.frame
+        let bounds = imageView.bounds
+
+        frameXLabel.text = "frame x = \(frame.origin.x)"
+        frameYLabel.text = "frame y = \(frame.origin.y)"
+
+        boundsXLabel.text = "bounds x = \(bounds.origin.x)"
+        boundsYLabel.text = "bounds y = \(bounds.origin.y)"
     }
 
+    private func updateSliders() {
+        let frame = imageView.frame
+        let bounds = imageView.bounds
+
+        frameXSlider.maximumValue = Float(frame.size.width)
+        frameYSlider.maximumValue = Float(frame.size.height)
+        frameXSlider.value = Float(frame.origin.x)
+        frameYSlider.value = Float(frame.origin.y)
+
+        boundsXSlider.maximumValue = Float(bounds.size.width)
+        boundsYSlider.maximumValue = Float(bounds.size.height)
+        boundsXSlider.value = Float(bounds.origin.x)
+        boundsYSlider.value = Float(bounds.origin.y)
+    }
 
 }
-
